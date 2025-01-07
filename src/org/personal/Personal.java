49051c682daa69067ff2d9e36877d3ae4c145b1e
shankarranganathan <shankarmech100@gmail.com>
2025-01-07 05:32:57 +0530

    personal commit

---
+++ src/org/personal/Personal.java
@@ -0,0 +1,31 @@
+package org.personal;
+
+public class Personal {
+	private void name() {
+		System.out.println("shankar");
+	}
+	private void wifeName() {
+		System.out.println("priya");
+
+	}
+	private void myJob() {
+		System.out.println("Tnwas AE");
+	}
+	private void wifeJob() {
+		System.out.println("goverment doctor");
+
+	}
+	private void childName() {
+		System.out.println("jai, vetri");
+
+	}
+	public static void main(String[] args) {
+		Personal p=new Personal();
+		p.name();
+		p.wifeName();
+		p.myJob();
+		p.wifeJob();
+		p.childName();
+	}
+
+}
